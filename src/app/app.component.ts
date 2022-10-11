03fd9230608dd22bb79b3e4afb58dcb2572dea0e
appalse <frontend-hh@yandex.ru>
2022-10-11 11:28:47 +0200

    add new components, save dark mode preference in local storage, routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,23 @@ export class AppComponent implements OnInit {
   @ViewChild("mainContainer") divMainContainer: ElementRef;
 
   ngOnInit(): void {
-    console.log(this.navBar);
+    const prefersDarkScheme = window.matchMedia("(prefers-color-schee: dark)");
+    const prefersDarkMode = prefersDarkScheme.matches; 
+    let isDarkMode = prefersDarkMode || localStorage.getItem('color-theme') === 'dark';
+    // call in timeout (not immediately but after current script) 
+    // because 'nativeElement' inside 'toggleDarkMode' function will be rendered after 'ngOnInit'
+    setTimeout(() => { this.toggleDarkMode(isDarkMode) });
   }
 
   toggleDarkMode(isDarkMode: boolean) {
     if (isDarkMode) {
       this.divMainContainer.nativeElement.classList.add("dark");
       document.body.style.backgroundColor = 'black';
+      localStorage.setItem('color-theme', 'dark');
     } else {
       this.divMainContainer.nativeElement.classList.remove("dark");
       document.body.style.backgroundColor = 'white';
+      localStorage.setItem('color-theme', 'light');
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
@@ -10,6 +11,9 @@ import { TeamComponent } from './components/main/team/team.component';
 import { ReviewsComponent } from './components/main/reviews/reviews.component';
 import { ContactFormComponent } from './components/main/contact-form/contact-form.component';
 import { PricingTableComponent } from './components/main/pricing-table/pricing-table.component';
+import { ServiceComponent } from './components/service/service.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
@@ -21,10 +25,19 @@ import { PricingTableComponent } from './components/main/pricing-table/pricing-t
     TeamComponent,
     ReviewsComponent,
     ContactFormComponent,
-    PricingTableComponent
+    PricingTableComponent,
+    ServiceComponent,
+    PricingComponent,
+    ContactsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'service', component: ServiceComponent},
+      { path: 'pricing', component: PricingComponent},
+      { path: 'contacts', component: ContactsComponent},
+      { path: '', component: MainComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
